631a989c625a3365ff4a555db130b61cbfaaa907
Jethet <henriette.hettinga@gmail.com>
2020-10-09 15:47:30 +0200

    Fix merge errors

--- Task3/js/updateText.js
+++ Task3/js/updateText.js
@@ -3,44 +3,70 @@ window.onload = function () {
   const textContainerDIV = document.getElementById("text_container");
   const textInput = document.getElementById("text_input");
   const errorCounterSPAN = document.getElementById("error_counter");
-  let errorCount = 0;
+  const accuracyCounterSPAN = document.getElementById("accuracy_counter");
+  var errorCount = 0;
+  var temp_mistakes = 0;
+  var total_errors = 0;
+  var charTyped = 0;
   var randomText = [
     "ine neit ent eletter lient ener lent tete ener tree neer teet ener neit ree tell ree neerine ere ree ret lier teen nient tell",
     "nient eletter ree treet ene eler neerit neer tree eler nient teel nient lier ener lent ener tell teen nient ree nient lettine",
     "let nient ret iner rel ree lier let neer rent elette let ine neerine nient teen ene relie ree teen elet ine ree elet reet teen",
   ];
-
   generator.onclick = function () {
     let textLength = Math.floor(Math.random() * randomText.length);
     currentText = randomText[textLength];
+    textInput.value = "";
+    errorCounterSPAN.innerText = 0;
+    accuracyCounterSPAN.innerText = 100 + "%";
     textContainerDIV.innerText = "";
+    total_errors = 0;
+    charTyped = 0;
+
     currentText.split("").forEach((character) => {
       const charSpan = document.createElement("span");
       //console.log(character);
+
       charSpan.innerText = character;
       textContainerDIV.append(charSpan);
 
-      // add the code to start the timer when the 'Generate' button is clicked
+      // call the function to start the timer when the 'Generate' button is clicked
       runTime();
     });
   };
 
-  // code for error count
-  textInput.onkeypress = (event) => {
-    if (event.key === " " || event.key === "Spacebar") {
-      const textContainerWords = textContainerDIV.innerText.split(" ");
-      const inputWords = textInput.value.split(" ");
+  // code for error count and accuracy
+  textInput.onkeypress = () => {
+    charTyped++;
+    textContainerWords = textContainerDIV.innerText.split("");
+    inputWords = textInput.value.split("");
+    str = "";
+    inputWords.forEach((char, index) => {
+      let typedChar = textContainerWords[index];
 
-      const lastIndex = inputWords.length - 1;
-      const lastWord = inputWords[lastIndex];
-      if (textContainerWords[lastIndex]) {
-        if (lastWord !== textContainerWords[lastIndex]) errorCount++;
-      } else errorCount++;
-      errorCounterSPAN.innerText = errorCount;
+      if (typedChar !== char) {
+        errorCount++;
+        textInput.style.color = "#ff0000";
+      } else {
+        textInput.style.color = "#000000";
+      }
+    });
+
+    errorCounterSPAN.innerText = errorCount;
+    if (errorCount > temp_mistakes) {
+      total_errors++;
+      //errorCount = temp_mistakes;
+      //total_errors+=temp_mistakes;
     }
+    let accuracy = ((charTyped - total_errors) / charTyped) * 100;
+    temp_mistakes = errorCount;
+    errorCount = 0;
+    console.log(total_errors);
+    accuracyCounterSPAN.innerText = Math.round(accuracy) + "%";
   };
 };
 
+// CODE FOR TIMER
 // function for digital formating of time
 function formatTime(num) {
   if (num < 10) {
@@ -70,7 +96,7 @@ function runTime() {
   // code to stop running time if button is clicked
   var stopButton = document.getElementById("stop-time-button");
   stopButton.addEventListener("click", function (e) {
-    e.preventDefault()
+    e.preventDefault();
     clearInterval(countTime);
     document.querySelector("#hours-passed").innerHTML = formatTime(0);
     document.querySelector("#minutes-passed").innerHTML = formatTime(0);
