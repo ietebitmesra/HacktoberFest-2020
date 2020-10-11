0a2d2f0df3dc2d9c55b3888015365914f2142462
coastaldemigod <shivanshagrawal2411@gmail.com>
2020-10-11 12:38:02 +0530

    Texts updated , alert message on completion added , css updated

--- Task3/js/updateText.js
+++ Task3/js/updateText.js
@@ -9,22 +9,23 @@ window.onload = function () {
     var total_errors = 0;
     var charTyped = 0;
     var randomText = [
-        "Peter Piper picked a peck of pickled peppers. How many pickled peppers did Peter Piper pick?",
         "The creatures outside looked from pig to man, and from man to pig, and from pig to man again; but already it was impossible to say which was which.",
         "Very few castaways can claim to have survived so long at sea as Mr. Patel, and none in the company of an adult Bengal tiger.",
         "Real courage is when you know you’re licked before you begin, but you begin anyway and see it through no matter what.",
-        "It’s the possibility of having a dream come true that makes life interesting.",
-        "It’s only after we’ve lost everything that we’re free to do anything.",
         "Recovering from suffering is not like recovering from a disease. Many people don’t come out healed; they come out different.",
-        "It is a great misfortune to be alone, my friends; and it must be believed that solitude can quickly destroy reason.",
-        "Some birds are not meant to be caged, that's all. Their feathers are too bright, their songs too sweet and wild. So you let them go, or when you open the cage to feed them they somehow fly out past you. And the part of you that knows it was wrong to imprison them in the first place rejoices, but still, the place where you live is that much more drab and empty for their departure.",
-        "The goal isn’t to live forever, the goal is to create something that will.",
-        "Harry Potter was a highly unusual boy in many ways. For one thing, he hated the summer holidays more than any other time of year. For another, he really wanted to do his homework, but was forced to do it in the dead of the night. And he also happened to be a wizard.",
-        "Ed likes seals sleek as silk. Fiji fields aid all feels adelia sail idle alkali seas said eddie.",
+        "It is a great misfortune to be alone, my friends; and it must be believed that solitude can quickly destroy reason",
         "It is late. Kate strolls out of a dark forest. Kate took a leaf for Dad. Kate also took a daffodil for Flor.",
-        "What lies behind us and what lies before us are small matters compared to what lies within us.",
-        "You will never change your life until you change something you do daily. The secret of yor success is found in your daily routine."
-
+        "You will never change your life until you change something you do daily. The secret of your success is found in your daily routine.", 	
+        "India is not, as people keep calling it, an underdeveloped country, but rather, in the context of its history and cultural heritage, a highly developed one in an advanced state of decay.",
+        "The ultimate measure of a man is not where he stands in moments of comfort and convenience, but where he stands at times of challenge and controversy.",
+        "I slept and dreamt that life was joy. I awoke and saw that life was service. I acted and behold, service was joy.",
+        "If I can't make it through one door, I'll go through another door- or I'll make a door. Something terrific will come no matter how dark the present.",
+        "For neither good nor evil can last for ever; and so it follows that as evil has lasted a long time, good must now be close at hand.",
+        "The most perceptive character in a play is the fool, because the man who wishes to seem simple cannot possibly be a simpleton.",
+        "There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.",
+        "When you are courting a nice girl, an hour seems like a second. When you sit on a red-hot cinder a second seems like an hour. That's relativity.",
+        "The most beautiful experience we can have is the mysterious. It is the fundamental emotion that stands at the cradle of true art and true science.",
+        "We keep moving forward, opening new doors, and doing new things, because we’re curious and curiosity keeps leading us down new paths."
     ]
     generator.onclick = function () {
         let textLength = Math.floor(Math.random() * (randomText.length));
@@ -40,7 +41,8 @@ window.onload = function () {
         total_errors=0
         charTyped=0
 
-
+        document.getElementById("text_input").setAttribute("rows", "3");
+        
         currentText.split('').forEach(character => {
             const charSpan = document.createElement('span')
             //console.log(character);
@@ -58,6 +60,10 @@ window.onload = function () {
         textContainerWords = textContainerDIV.innerText.split('');
         inputWords = textInput.value.split('');
         str='';
+        
+        if(textInput.value.localeCompare(textContainerDIV.innerText)==0 )
+        alert(" Congratulations on completing the typing test. ");
+
         inputWords.forEach((char, index) => {
 
             let typedChar = textContainerWords[index]
@@ -70,9 +76,8 @@ window.onload = function () {
                     textInput.style.color='#000000';
              }
 
-
         });
-
+        
             errorCounterSPAN.innerText = errorCount;
             if(errorCount > temp_mistakes){
                 total_errors++;
